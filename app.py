dc9988cf27e38cdc8f5e3fdaaa9e7d52d3b6dc45
Rob Mawston <robmaw@microsoft.com>
2024-09-18 09:38:02 -0700

    rework transforms

--- app.py
+++ app.py
@@ -21,8 +21,39 @@ trace.set_tracer_provider(traceProvider)
 # Creates a tracer from the global tracer provider
 tracer = trace.get_tracer("my.tracer.name")
 
+def calling_autoinstrumented_libA():
+        with tracer.start_as_current_span("auto-instrumented-span-to-suppress") as span:
+          span.set_attribute("app_name", "libA")
+          span.set_attribute("api_key", "123456789")
+          span.add_event("Event 1: Calling API")
+          span.add_event("Event 2: Returned 123456")
+    
+def calling_autoinstrumented_libB():
+        with tracer.start_as_current_span("another-span-to-suppress") as span:
+          span.set_attribute("app_name", "libB")
+          span.set_attribute("api_key", "123456789")
+          span.add_event("Event 1: Calling API")
+          span.add_event("Event 2: Returned 123456")
+    
+def calling_autoinstrumented_libC():
+        with tracer.start_as_current_span("yet-another-span-to-suppress") as span:
+          span.set_attribute("app_name", "libC")
+          span.set_attribute("api_key", "123456789")
+          span.add_event("Event 1: Calling API")
+          span.add_event("Event 2: Returned 123456")
+          with tracer.start_span("subspan-of-yet-another-span-to-suppress") as subspan:
+            subspan.set_attribute("app_name", "libC")
+            subspan.set_attribute("api_key", "123456789")
+            subspan.add_event("Event 1: Calling API")
+            subspan.add_event("Event 2: Returned 123456")
+
+def calling_autoinstrumented():
+    calling_autoinstrumented_libA()
+    calling_autoinstrumented_libB()
+    calling_autoinstrumented_libC()
+
 def do_work():
-    with tracer.start_as_current_span("parent-span") as span:
+    with tracer.start_as_current_span("oblivious-to-sensitivity") as span:
         span.set_attribute("app_name", "test")
         span.set_attribute("user_input", "My tax file number is 123456789")
         span.add_event("Event 1: TFN in attributes", {"tfn": "123456789"})
@@ -34,14 +65,15 @@ def mixed_function():
     with tracer.start_as_current_span("known-span-with-sensitive-attributes") as span:
         span.set_attribute("user", "someone")
         span.add_event("Event 3: No sensitive data", {"foo": "bar"})
-        span.add_event("Event 4: Explicit PII event", {"foo": "bar", "has_pii": "true"})
+        span.add_event("Event 4: Explicit PII event", {"foo": "bar", "is_sensitive": "true"})
         print("simulating a mixed function which has a sensitive attributes...")
         log_ppi_data()
 
 def log_ppi_data():
-    with tracer.start_as_current_span("known-sensitive-span", attributes={"has_pii": "true"}) as span:
+    with tracer.start_as_current_span("known-sensitive-span", attributes={"is_sensitive": "true"}) as span:
         span.add_event("Event 5: Implicit PII event", {"foo": "bar"})
         print("doing some work with PII...")
 
 if __name__ == "__main__":
-    do_work()
+    # do_work()
+    calling_autoinstrumented()
